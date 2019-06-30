063dba0bbf34fe9f81a305ff69e747f0704a93d2
Chad Orwig <chad.orwig@slalom.com>
2019-06-30 11:40:30 -0400

    build for prod

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,59 +1,22 @@
-const { VueLoaderPlugin } = require('vue-loader');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpack = require('webpack');
+const prodConfig = require('./webpack.config.prod');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
-    mode: 'development',
-    devtool: 'cheap-module-eval-source-map',
-    entry: [
-        './src/index.js'
-    ],
-    target : 'web',
-    output: {
-        path       : __dirname + '/build',
-        publicPath : '/',
-        filename   : "bundle.js"
-    },
-    plugins : [
-        new HtmlWebpackPlugin({
-            template: './src/index.html',
-            hot: true
-        }),
-        new VueLoaderPlugin(),
-        new webpack.NamedModulesPlugin(),
-        new webpack.HotModuleReplacementPlugin()
-    ],
-    devServer: {
-        contentBase: __dirname + '/build',
-        historyApiFallback : {
-            index : 'index.html'
-        }
-    },
-    module : {
-        rules : [
-            { 
-                test: /\.vue$/,
-                use: 'vue-loader'
-            },
-            {
-                test : /\.css$/,
-                use : [
-                    'vue-style-loader',
-                    'css-loader'
-                ]
-            },
-            {
-                test : /\.sass$/,
-                use : [
-                    'vue-style-loader',
-                    'css-loader',
-                    'sass-loader'
-                ]
-            },
-            {
-                test : /\.svg$|\.eot$|\.woff$|\.woff2$|\.ttf$|\.png$/,
-                use : 'file-loader'
-            }
-        ]
+prodConfig.plugins.push(
+    new HtmlWebpackPlugin({
+        template: './src/index.html',
+        hot: true
+    }));
+
+prodConfig.plugins.push(
+    new webpack.HotModuleReplacementPlugin()
+);
+
+prodConfig.devServer = {
+    contentBase: __dirname + '/build',
+    historyApiFallback : {
+        index : 'index.html'
     }
 };
+
+module.exports = prodConfig;
---
+++ webpack.config.prod.js
@@ -0,0 +1,47 @@
+const { VueLoaderPlugin } = require('vue-loader');
+const webpack = require('webpack');
+
+module.exports = {
+    mode: 'development',
+    devtool: 'cheap-module-eval-source-map',
+    entry: [
+        './src/index.js'
+    ],
+    target : 'web',
+    output: {
+        path       : __dirname + '/build',
+        publicPath : '/',
+        filename   : "bundle.js"
+    },
+    plugins : [
+        new VueLoaderPlugin(),
+        new webpack.NamedModulesPlugin()
+    ],
+    module : {
+        rules : [
+            { 
+                test: /\.vue$/,
+                use: 'vue-loader'
+            },
+            {
+                test : /\.css$/,
+                use : [
+                    'vue-style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test : /\.sass$/,
+                use : [
+                    'vue-style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            },
+            {
+                test : /\.svg$|\.eot$|\.woff$|\.woff2$|\.ttf$|\.png$/,
+                use : 'file-loader'
+            }
+        ]
+    }
+};
